529bea1f89ea95178d5b6745fbeab345b276a18d
David Chmura <dschmura@humbledaisy.com>
2018-02-19 12:56:12 -0500

    UPDATE: Puma uploads to production should pull from the sample.
    This allows the default developer config to work.

--- config/deploy.rb
+++ config/deploy.rb
@@ -76,7 +76,7 @@ end
     on roles (:app) do
       upload! "config/master.key",  "#{shared_path}/config/master.key"
       # upload! "config/secrets.yml.key",  "#{shared_path}/config/secrets.yml.key"
-      upload! "config/puma.rb",  "#{shared_path}/config/puma.rb"
+      upload! "config/puma.sample.rb",  "#{shared_path}/config/puma.rb"
       upload! "config/nginx.conf",  "#{shared_path}/config/nginx.conf"
     end
   end
