4be5c375a06635e45079c7dde04577e66d6a3fa9
David Chmura <dschmura@humbledaisy.com>
2018-04-07 09:27:47 -0400

    UPDATE: prepping for deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+  # Verifies that versions and hashed value of the package contents in the project's package.json
+    config.webpacker.check_yarn_integrity = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              Rails.application.credentials.DAVECHMURA_WEBSITE_EMAIL_SERVER,
